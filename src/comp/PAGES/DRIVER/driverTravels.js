42846ffa298ba8fd0c321937f756e6a51a56eda3
Rivka <rivky939@gmail.com>
2023-07-17 14:30:55 +0300

    DriverTravels

--- src/comp/PAGES/DRIVER/driverTravels.js
+++ src/comp/PAGES/DRIVER/driverTravels.js
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from "react";
 import TravelService from "../../SERVICES/TravelService";
 import { Box, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
+import CardTravel from "../PASSENGER/cardTravel";
+
 export default function DriverTravels() {
   const data = useSelector((state) => state);
-  const userId=data.id
+  const userId = data.id;
+  const [searchTravel, setSearchTravel] = useState([{
+    SourceCity: "קרית ספר",
+    SourceStreet: "",
+    SourceHouseNumber: "1",
+  },{
+    DestCity: "בני ברק",
+    DestStreet: "",
+    DestHouseNumber: "1",
+  },{
+    TimeTravel: new Date(),
+    freeSpace: 1,
+  }]);
   const [futureTravelsByUserId, setFutureTravelsByUserId] = useState([]);
   const [pastTravelsByUserId, setPastTravelsByUserId] = useState([]);
   useEffect(() => {
@@ -12,15 +26,83 @@ export default function DriverTravels() {
     //     setFutureTravelsByUserId(res);
     // });
     TravelService.getPastTravelsByUserId(userId).then((res) => {
-        setPastTravelsByUserId(res);
+      setPastTravelsByUserId(res);
     });
   }, []);
 
   return (
-    <>
-      <h1>DriverTravels</h1>
-      {/* לעבור על הרשימה map ולהציג כל איבר */}
-      <Box style={{ display: "flex" }}>
+    <Box style={{}}>
+      <Box
+        style={{
+          marginTop: "30px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Typography color="primary" variant="h4">
+          Driver Travels
+        </Typography>
+      </Box>
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap:'wrap',
+
+        }}
+      >
+        <Box
+          style={{ display: "flex",
+            padding: "15px" }}
+        >
+          <Typography color="primary" variant="h6">
+            past Travel:
+          </Typography>
+          {searchTravel.map((travel) => {
+          return (
+            <Box style={{ margin: "20px" }}>
+              <CardTravel
+                data={travel}
+                setSearchTravel={setSearchTravel}
+                searchTravel={searchTravel}
+              />
+            </Box>
+          );
+          // <Typography key={travel.id}>
+          //   {travel.sourceCity}
+          //   {travel.sourceStreet}
+          // </Typography>
+        })}
+        </Box>
+        <Box
+          style={{
+            display: "flex",
+            // backgroundColor: "yellow",
+            padding: "15px",
+          }}
+        >
+          <Typography color="primary" variant="h6">
+            Future Travel:
+          </Typography>
+          {searchTravel.map((travel) => {
+          return (
+            <Box style={{ margin: "20px" }}>
+              <CardTravel
+                data={travel}
+                setSearchTravel={setSearchTravel}
+                searchTravel={searchTravel}
+              />
+            </Box>
+          );
+          // <Typography key={travel.id}>
+          //   {travel.sourceCity}
+          //   {travel.sourceStreet}
+          // </Typography>
+        })}
+        </Box>
+        {/* <h1>DriverTravels</h1> */}
+        {/* לעבור על הרשימה map ולהציג כל איבר */}
+        {/* <Box style={{ display: "flex" }}> */}
         {/* <Typography color="primary" variant="h6">
           All futureTravelsByUserId:
           {futureTravelsByUserId.map((travel) => (
@@ -28,13 +110,7 @@ export default function DriverTravels() {
           ))}
         </Typography> */}
         {/* xxxx */}
-        <Typography color="primary" variant="h6">
-          past Travel:
-          {pastTravelsByUserId.map((travel) => (
-            <Typography key={travel.id}>{travel.sourceCity}{travel.sourceStreet}</Typography>
-          ))}
-        </Typography>
       </Box>
-    </>
+    </Box>
   );
 }
